4e03be7c03cdf2d4bbb3e1497d8797a89a403c93
cejezed <cejezed@gmail.com>
2025-09-10 09:54:58 +0000

    Fix Planning.tsx syntax

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import { AuthProvider } from "@/lib/auth.tsx;
+import { AuthProvider } from "@/lib/auth.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
